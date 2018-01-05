a61939d802846383d15265e6967db5f71dcfee9a
James <jamezhall@gmail.com>
2018-01-05 22:38:26 +1100

    0.16 entity graphics update

--- prototypes/entity/cheap-solar.lua
+++ prototypes/entity/cheap-solar.lua
@@ -17,11 +17,64 @@ data:extend(
     },
     picture =
     {
-      filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/cheap-solar.png",
-      priority = "high",
-      width = 104,
-      height = 96
+      layers =
+      {
+        {
+          filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/cheap-solar.png",
+          priority = "high",
+          width = 116,
+          height = 112,
+          shift = util.by_pixel(-3, 3),
+          hr_version = {
+            filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/hr-cheap-solar.png",
+            priority = "high",
+            width = 230,
+            height = 224,
+            shift = util.by_pixel(-3, 3.5),
+            scale = 0.5
+          }
+        },
+        {
+          filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/cheap-solar-shadow.png",
+          priority = "high",
+          width = 112,
+          height = 90,
+          shift = util.by_pixel(10, 6),
+          draw_as_shadow = true,
+          hr_version = {
+            filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/hr-cheap-solar-shadow.png",
+            priority = "high",
+            width = 220,
+            height = 180,
+            shift = util.by_pixel(9.5, 6),
+            draw_as_shadow = true,
+            scale = 0.5
+          }
+        }
+      }
     },
+    overlay =
+    {
+      layers =
+      {
+        {
+          filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/cheap-solar-shadow-overlay.png",
+          priority = "high",
+          width = 108,
+          height = 90,
+          shift = util.by_pixel(11, 6),
+          hr_version = {
+            filename = "__Pro Fun Cheap Solar__/graphics/cheap-solar/hr-cheap-solar-shadow-overlay.png",
+            priority = "high",
+            width = 214,
+            height = 180,
+            shift = util.by_pixel(10.5, 6),
+            scale = 0.5
+          }
+        }
+      }
+    },
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
     production = "60MW"
   }
 }
